3b6f1428be428fc945b57aa0cf47b4e6bedbcacb
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-15 20:44:06 -0500

    list

---
+++ List/Linked-list.js
@@ -0,0 +1,27 @@
+class Node{
+    constructor(value,nextNode){
+        this.value = null;
+        this.nextNode = null;
+    }
+}
+
+export class LinkedList{
+    constructor(list){
+    this.head = null;
+    this.size = 0;
+    this.tail = null;
+    
+
+    }
+
+     append(value){
+        const newnode = new Node(value);
+        }
+
+
+    prepend(value){
+
+    }
+
+}
+
